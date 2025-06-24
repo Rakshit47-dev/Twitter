903afd417455f854d95530a6cf7b7a7901c9acd1
Rakshit Sharma <rsharma@academian.com>
2025-06-24 15:04:51 +0530

    sixth commit

--- .history/frontend/Twitter/src/services/api_20250624150242.js
+++ .history/frontend/Twitter/src/services/api_20250624150242.js
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-
+  baseURL: baseURL: import.meta.env.VITE_API_URL,
+, 
   withCredentials: true, 
 });
 
